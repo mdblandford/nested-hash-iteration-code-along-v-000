35edd302cc2ddc9871919b44fd6aff3af0096278
Michael Blandford <mdblandford@gmail.com>
2018-06-21 16:27:13 +0000

    Done.

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -3,16 +3,16 @@ def remove_strawberry(contacts)
   contacts = {
      "Jon Snow" => {
        name: "Jon",
-       email: "jon_snow@thewall.we", 
+       email: "jon_snow@thewall.we",
        favorite_ice_cream_flavors: ["chocolate", "vanilla"]},
      "Freddy Mercury" => {
        name: "Freddy",
        email: "freddy@mercury.com",
        favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]}}
-   contacts.each do |person, data|
-     data.each do |attribute, item|
-       if attribute == :favorite_ice_cream_flavors
-         item.delete_if {|flavor| flavor == "strawberry"}
+   contacts.each do |a, b|
+     b.each do |c, d|
+       if c == :favorite_ice_cream_flavors
+         d.delete_if {|flavor| flavor == "strawberry"}
        end
      end
    end
